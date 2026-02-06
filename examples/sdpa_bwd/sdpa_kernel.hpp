7fb7b8c9012f9defa7c92f74fde750fee5104314
Yuankun Shi <yuankun.shi@intel.com>
2026-02-05 17:16:03 -0800

    isolate kernel into header files

---
+++ examples/sdpa_bwd/sdpa_kernel.hpp
@@ -0,0 +1,99 @@
+#pragma once
+#include <sycl/sycl.hpp>
+#include <cute/util/compat.hpp>
+#include <cassert>
+#include <cute/tensor.hpp>
+
+#include "cutlass/gemm/device/gemm_universal.h"
+#include "cutlass/util/reference/device/gemm_complex.h"
+#include "cutlass/util/print_error.hpp"
+#include "cutlass/util/sycl_event_manager.hpp"
+#include "cutlass/util/GPU_Clock.hpp"
+#include "params.hpp"
+
+#include "sdpa_backward.hpp"
+#include "sdpa_preprocessing.hpp"
+
+void read_args(int argc, char**argv, int n, int64_t *p) {
+    if (argc >= n + 1)
+        sscanf(argv[n], "%ld", p);
+}
+
+void
+debug_info() {
+    print("block idx (%d,%d,%d) dim (%d,%d,%d) thread idx (%d,%d,%d) dim (%d,%d,%d)\n",
+          BlockIdxX(), BlockIdxY(), BlockIdxZ(),
+          GridDimX(), GridDimY(), GridDimZ(),
+          ThreadIdxX(), ThreadIdxY(), ThreadIdxZ(),
+          BlockDimX(), BlockDimY(), BlockDimZ());
+}
+
+template<class Engine, class Layout>
+void print_t(Tensor<Engine, Layout> &r) {
+    print(r);
+    for (int i = 0; i < size(r); ++i) {
+        if (i % 8 == 0)
+            print("\n(%03d): ", i / 8);
+        print("%10.7f ", (float)r(i));
+    }
+    print("\n");
+}
+
+template<class Engine, class Layout, class TVLayout>
+void print_t(SubgroupTensor<Engine, Layout, TVLayout> &r) {
+    print(r);
+    for (int i = 0; i < size(r.tensor()); ++i) {
+        if (i % 8 == 0)
+            print("\n(%03d): ", i / 8);
+        print("%10.7f ", (float)r(i));
+    }
+    print("\n");
+}
+
+template<class T1, class T2>
+void print_t(T1 m, T2 g) {
+    print(m);
+    for (int i = 0; i < size(g); ++i) {
+        if (i % 8 == 0)
+            print("\n(%03d): ", i / 8);
+        print("%10.7f ", (float)m(g(i)));
+    }
+    print("\n");
+}
+
+template<class T>
+void print_t_2d(T t) {
+    static_assert(rank(t) == 2, "Only support 2D Tensor");
+    print(t);
+    for (int i = 0; i <  size < 0>(t); ++i) {
+        print("\n(%03d): ", i);
+        for (int j = 0; j < size<1>(t); ++j) {
+            print("%10.7f ", (float)t(i,j));
+        }
+    }
+    print("\n");
+}
+
+template<class T>
+void print_d(T t) {
+    print(t);
+    for (int i = 0; i < size(t); ++i) {
+        if (i % 8 == 0)
+            print("\n(%03d): ", i / 8);
+        print("%10u ", t(i));
+    }
+    print("\n");
+}
+
+template<class T>
+void print_c(T t) {
+    print(t);
+    for (int i = 0; i < size(t); ++i) {
+        if (i % 8 == 0)
+            print("\n(%03d): ", i / 8);
+        print(t(i));
+    }
+    print("\n");
+}
+
+using ProblemShapeRegular = cute::tuple<int, int, int, int, int, int, int>; // batch, num_head_q,num_head_kv,seq_len_qo,seq_len_kv,head_size_qk,head_size_vo
